f837e3a1eca24554dbfc184ea10cb071f21e47c5
dsafonov <Softik.d@gmail.com>
2025-08-31 12:25:57 +0300

    REF: Изменено  время задержки срабатывания  датчиков пересечения и минимальное время круга до 1 сек

--- include/config.h
+++ include/config.h
@@ -28,8 +28,8 @@ void saveWiFiSettings();
 
 #define SENSOR1_PIN 14
 #define SENSOR2_PIN 27
-#define DEBOUNCE_TIME 5 * 1000000 // задержка от срабатывания датчиков пересечения 5сек
-#define MIN_LAP_TIME 3000000
+#define DEBOUNCE_TIME 1.0 * 1000000 // задержка от срабатывания датчиков пересечения 1сек
+#define MIN_LAP_TIME 1.0 * 1000000
 #define HISTORY_SIZE 5
 #define DATA_UPDATE_INTERVAL 100 // Интервал обновления данных (мс)
 
